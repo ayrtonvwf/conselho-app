b7b7fe33ec5f553976ac59ad331a81d888f4bb17
Ayrton Fidelis <ayrton.vargas33@gmail.com>
2018-07-31 15:52:06 -0300

    Fix left column of evaluations

--- js/super-table.js
+++ js/super-table.js
@@ -2,9 +2,12 @@ let table
 let table_header
 let horizontal_scroll
 let fixed_table
+let fixed_column
 let right_interval
 let left_interval
 let is_touching = false
+let ignore_mutation = false
+let observer
 
 function dettectTouchStart() {
     is_touching = true
@@ -71,6 +74,21 @@ function positionTableHeader() {
     fixed_table.style.marginTop = (-offset)+'px'
 }
 
+function positionTableCol() {
+    if (!fixed_column) {
+        return
+    }
+
+    let offset = horizontal_scroll.scrollLeft
+    if (!offset) {
+        fixed_column.style.display = 'none'
+        return
+    }
+
+    fixed_column.style.display = 'table'
+    fixed_column.style.marginLeft = offset+'px'
+}
+
 function calculateTableHeaderWidth() {
     let ths = table_header.querySelectorAll('th')
     ths.forEach((th, i) => {
@@ -81,9 +99,60 @@ function calculateTableHeaderWidth() {
     })
 }
 
+function mountFixedColumn() {
+    let prev_fixed_col = document.querySelector('[data-fixed_column]')
+    if (prev_fixed_col) {
+        horizontal_scroll.removeChild(prev_fixed_col)
+    }
+
+    fixed_column = fixed_table.cloneNode(false)
+    fixed_column.style.top = '10px' // .table margin
+    fixed_column.style.marginTop = '0px'
+    fixed_column.style.borderRight = '1px solid darkgray'
+    fixed_column.dataset.fixed_column = 'fixed_column'
+    let fixed_column_tbody = table.querySelector('tbody')
+    if (fixed_column_tbody) {
+        fixed_column_tbody = fixed_column_tbody.cloneNode()
+        let fixed_column_thead = fixed_table.querySelector('thead').cloneNode()
+        let fixed_column_thead_tr = fixed_table.querySelector('tr').cloneNode()
+        let fixed_column_thead_th = fixed_table.querySelector('th').cloneNode(true)
+        let original_th = table.querySelector('th')
+
+        fixed_column.style.width = original_th.offsetWidth+'px'
+
+        let height = original_th.offsetHeight+'px'
+
+        fixed_column_thead_th.style.height = height
+        fixed_column_thead_th.style.minHeight = height
+        fixed_column_thead_th.style.maxHeight = height
+
+        fixed_column_thead_tr.appendChild(fixed_column_thead_th)
+        fixed_column_thead.appendChild(fixed_column_thead_tr)
+        fixed_column.appendChild(fixed_column_thead).cloneNode()
+        table.querySelectorAll('tbody>tr').forEach((tr, i) => {
+            let pos = i+1
+            let fixed_column_tbody_tr = tr.cloneNode()
+            let fixed_column_tbody_td = tr.querySelector('td').cloneNode(true)
+            let height = table.querySelector('tbody>tr:nth-child('+pos+')>td').offsetHeight+'px'
+
+            fixed_column_tbody_td.style.height = height
+            fixed_column_tbody_td.style.minHeight = height
+            fixed_column_tbody_td.style.maxHeight = height
+
+            fixed_column_tbody_tr.appendChild(fixed_column_tbody_td)
+            fixed_column_tbody.appendChild(fixed_column_tbody_tr)
+        })
+        fixed_column.appendChild(fixed_column_tbody)
+        horizontal_scroll.appendChild(fixed_column)
+    }
+}
+
 function superTable() {
     table = document.querySelector('table:not([data-fixed_table])')
     horizontal_scroll = document.querySelector('.table')
+    if (observer) {
+        observer.disconnect()
+    }
 
     if (!table || !horizontal_scroll) {
         return
@@ -100,29 +169,44 @@ function superTable() {
     fixed_table.style.backgroundColor = 'white'
     fixed_table.style.display = 'none'
     fixed_table.style.top = '0px'
+    fixed_table.style.left = '0px'
     fixed_table.dataset.fixed_table = 'fixed_table'
     fixed_table.appendChild(table_header.cloneNode(true))
 
     calculateTableHeaderWidth()
+    mountFixedColumn()
 
     table.style.position = 'relative'
 
     horizontal_scroll.removeEventListener('mouseout', stopScrollOnMouseOut)
     horizontal_scroll.removeEventListener('mousemove', scrollOnMousePosition)
     window.removeEventListener('scroll', positionTableHeader)
+    horizontal_scroll.removeEventListener('scroll', positionTableCol)
 
     if (!is_touching) {
         horizontal_scroll.addEventListener('mousemove', scrollOnMousePosition)
         horizontal_scroll.addEventListener('mouseout', stopScrollOnMouseOut)
     }
     window.addEventListener('scroll', positionTableHeader)
+    horizontal_scroll.addEventListener('scroll', positionTableCol)
 
     horizontal_scroll.appendChild(fixed_table)
 
-    let observer = new MutationObserver(calculateTableHeaderWidth)
-    observer.observe(horizontal_scroll, {
+    let observer_options = {
         attributes: false,
         childList: true,
         subtree: true
+    }
+
+    observer = new MutationObserver(() => {
+        console.log('muted')
+
+        observer.disconnect()
+
+        calculateTableHeaderWidth()
+        mountFixedColumn()
+
+        observer.observe(horizontal_scroll, observer_options)
     })
+    observer.observe(horizontal_scroll, observer_options)
 }

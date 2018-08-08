2f3c5d65e2df6575a051deccf94e1d23168d4f3d
Ayrton Fidelis <ayrton.vargas33@gmail.com>
2018-08-08 20:11:52 -0300

    Decouple TeacherRequest component from App

--- src/components/pages/TeacherRequest.vue
+++ src/components/pages/TeacherRequest.vue
@@ -17,7 +17,7 @@
                 <div class="input col-6 col-sm-4">
                   <select id="current_subject_id" name="subject_id" required v-model="current_subject_id" :disabled="!current_grade_id">
                     <option value="" selected hidden disabled>{{ current_grade_id ? 'Selecione...' : 'Selecione a turma...' }}</option>
-                    <option v-for="subject in subjects" :key="subject.id" v-if="current_grade_id && grade_subjects.find(grade_subject => grade_subject.subject_id === subject.id && grade_subject.grade_id === current_grade_id) !== undefined" :value="subject.id" :disabled="teachers.find(teacher => teacher.grade_id === current_grade_id && teacher.subject_id === subject.id) !== undefined">{{ subject.name }}</option>
+                    <option v-for="subject in current_subjects" :key="subject.id" :value="subject.id" :disabled="current_teachers.find(teacher => teacher.subject_id === subject.id) || teacher_requests.find(teacher_request => teacher_request.grade_id === current_grade_id && teacher_request.subject_id === subject.id)">{{ subject.name }}</option>
                   </select>
                   <label for="current_subject_id">Disciplina</label>
                 </div>
@@ -30,28 +30,36 @@
             </div>
           </div>
         </form><br>
-        <table class="table" v-if="teachers.find(teacher => teacher.user_id === user.id) !== undefined || teacher_requests.find(teacher_request => teacher_request.user_id === user_id) !== undefined">
+        <table class="table" v-if="current_user_teachers.length || teacher_requests.length">
           <thead>
-          <tr>
-            <th>Turma</th>
-            <th>Disciplina</th>
-          </tr>
+            <tr>
+              <th>Turma</th>
+              <th>Disciplina</th>
+            </tr>
           </thead>
           <tbody>
-          <tr v-for="teacher_request in teacher_requests" :key="teacher_request.id" v-if="teacher_request.user_id === user.id">
-            <td>{{ grades.find(grade => grade.id === teacher_request.grade_id).name }}</td>
-            <td>{{ subjects.find(subject => subject.id === teacher_request.subject_id).name }}</td>
-            <td class="text-muted">Aguardando aprovação</td>
-            <td class="text-right no-wrap"><a class="btn-danger btn-sm tooltip tooltip-end" :href="'#modal-delete-teacher_request-'+teacher_request.id" title="Remover">
-              <div class="material-icons">delete</div><span class="d-none d-md-inline"> Remover</span></a></td>
-          </tr>
-          <tr v-for="teacher in teachers" :key="teacher.id" v-if="teacher.user_id === user.id">
-            <td>{{ grades.find(grade => grade.id === teacher.grade_id).name }}</td>
-            <td>{{ subjects.find(subject => subject.id === teacher.subject_id).name }}</td>
-            <td></td>
-            <td class="text-right no-wrap"><a class="btn-danger btn-sm tooltip tooltip-end" href="#modal-delete-teacher" title="Remover">
-              <div class="material-icons">delete</div><span class="d-none d-md-inline"> Remover</span></a></td>
-          </tr>
+            <tr v-for="teacher_request in teacher_requests" :key="'teacher_request-'+teacher_request.id">
+              <td>{{ grades.find(grade => grade.id === teacher_request.grade_id).name }}</td>
+              <td>{{ subjects.find(subject => subject.id === teacher_request.subject_id).name }}</td>
+              <td class="text-muted">Aguardando aprovação</td>
+              <td class="text-right no-wrap">
+                <a class="btn-danger btn-sm tooltip tooltip-end" href="#modal-delete-teacher_request" title="Remover" @click="current_teacher_request_id = teacher_request.id">
+                  <div class="material-icons">delete</div>
+                  <span class="d-none d-md-inline">Remover</span>
+                </a>
+              </td>
+            </tr>
+            <tr v-for="teacher in current_user_teachers" :key="'teacher-'+teacher.id">
+              <td>{{ grades.find(grade => grade.id === teacher.grade_id).name }}</td>
+              <td>{{ subjects.find(subject => subject.id === teacher.subject_id).name }}</td>
+              <td></td>
+              <td class="text-right no-wrap">
+                <a class="btn-danger btn-sm tooltip tooltip-end" href="#modal-delete-teacher" title="Remover">
+                  <div class="material-icons">delete</div>
+                  <span class="d-none d-md-inline">Remover</span>
+                </a>
+              </td>
+            </tr>
           </tbody>
         </table>
         <div class="text-center text-muted" v-else><br>
@@ -66,7 +74,7 @@
       <p>Caso seja realmente necessário, solicite ao supervisor que te desvincule.</p>
     </prompt>
 
-    <prompt v-for="teacher_request in teacher_requests" :key="teacher_request.id" title="Remover solicitação" type="danger" :anchor="'modal-delete-teacher_request-'+teacher_request.id" @accept="teacher_request_delete(teacher_request.id)" accept="Remover">
+    <prompt title="Remover solicitação" type="danger" anchor="modal-delete-teacher_request" @accept="teacher_request_delete" accept="Remover">
       <p>Tem certeza que deseja remover esta solicitação?</p>
     </prompt>
   </div>
@@ -77,41 +85,132 @@
 export default {
   name: 'TeacherRequest',
   data: function() {
-    return this.$parent.$data
+    return {
+      grades: [],
+      subjects: [],
+
+      grade_subjects: [],
+      teacher_requests: [], // only of the current teacher
+      teachers: [],
+
+      current_user_teachers: [],
+
+      current_user_id: undefined,
+      current_grade_id: '',
+      current_subject_id: '',
+      current_subjects: [],
+      current_teachers: []
+    }
+  },
+  watch: {
+    current_grade_id() {
+      if (!this.current_grade_id) {
+        this.current_subject_id = ''
+        this.current_subjects = []
+        this.current_teachers = []
+        return
+      }
+
+      let grade_subjects = this.grade_subjects.filter(grade_subject =>
+        grade_subject.grade_id === this.current_grade_id
+      )
+
+      this.current_subjects = this.subjects.filter(subject =>
+        grade_subjects.find(grade_subject =>
+          grade_subject.subject_id === subject.id
+        )
+      )
+
+      this.current_teachers = this.teachers.filter(teacher =>
+        teacher.grade_id === this.current_grade_id
+      )
+    }
   },
   methods: {
     teacher_request_save (event) {
-      event.preventDefault()
+      this.$emit('loading')
 
       let app = this.$parent
 
-      app.loading = true
-
       let form = event.target
+
       let data = {
-        grade_id: form.querySelector('[name=grade_id]').value,
-        subject_id: form.querySelector('[name=subject_id]').value,
-        user_id: app.user.id
+        grade_id: this.current_grade_id,
+        subject_id: this.current_subject_id,
+        user_id: this.current_user_id
       }
 
-      app.save_resource('teacher_request', data).then(() => {
-        app.loading = false
-        app.current_grade_id = ''
-        app.current_subject_id = ''
-        app.notify('Sucesso!', form.dataset.success, 'success')
+      app.save_resource('teacher_request', data, true, false).then(teacher_request => {
+        this.$emit('notify', 'Sucesso!', form.dataset.success, 'success')
+
+        this.teacher_requests.push(teacher_request)
+
+        this.current_subject_id = ''
       }).catch(error => {
-        app.loading = false
+        this.$emit('notify', 'Erro!', form.dataset.error, 'danger')
         console.log('Error:', error)
-        app.notify('Erro!', form.dataset.error, 'danger')
+      }).finally(() => {
+        this.$emit('loaded')
       })
     },
-    teacher_request_delete (teacher_request_id) {
-      this.$parent.delete_resource('teacher_request', teacher_request_id)
+    teacher_request_delete() {
+      this.$parent.delete_resource('teacher_request', this.current_teacher_request_id, true, false).then(() => {
+        let index = this.teacher_requests.findIndex(teacher_request =>
+          teacher_request.id === this.current_teacher_request_id
+        )
+        this.$delete(this.teacher_requests, index)
+      })
     }
   },
+  beforeCreate() {
+    this.$emit('loading')
+  },
   created: function () {
+    this.grades = []
+    this.subjects = []
+
+    this.grade_subjects = []
+    this.teacher_requests = []
+    this.teachers = []
+
+    this.current_user_teachers = []
+
+    this.current_user_id = this.$parent.token.user_id
     this.current_grade_id = ''
     this.current_subject_id = ''
+    this.current_subjects = []
+    this.current_teachers = []
+
+    let db = this.$parent.db
+    let promises = []
+    promises.push(db.grades.toArray().then(grades =>
+      this.grades = grades
+    ))
+    promises.push(db.subjects.toArray().then(subjects =>
+      this.subjects = subjects
+    ))
+
+    Promise.all(promises).then(() => {
+      let promises = []
+      promises.push(db.grade_subjects.toArray().then(grade_subjects =>
+        this.grade_subjects = grade_subjects
+      ))
+      promises.push(db.teacher_requests.toArray().then(teacher_requests =>
+        this.teacher_requests = teacher_requests.filter(teacher_request =>
+          teacher_request.user_id === this.current_user_id
+        )
+      ))
+      promises.push(db.teachers.toArray().then(teachers =>
+        this.teachers = teachers
+      ))
+
+      return Promise.all(promises)
+    }).then(() => {
+      this.current_user_teachers = this.teachers.filter(teacher =>
+        teacher.user_id === this.current_user_id
+      )
+      this.$emit('loaded')
+    })
   }
 }
 </script>

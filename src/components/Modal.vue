25969bdbdf5f1687a28f0560fb7addab7f1f7bd8
Ayrton Fidelis <ayrton.vargas33@gmail.com>
2018-08-21 18:35:26 -0300

    Close Modal and Prompt components on ESC keydown

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -19,10 +19,27 @@ export default {
       document.location.hash = this.anchor
       this.$emit('open')
     },
+
     close () {
       document.location.hash = this.$route.path
       this.$emit('close')
+    },
+
+    keyDown (event) {
+      if (event.keyCode !== 27) {
+        return
+      }
+
+      this.close()
     }
+  },
+
+  created () {
+    window.addEventListener('keydown', this.keyDown)
+  },
+
+  beforeDestroy () {
+    window.removeEventListener('keydown', this.keyDown)
   }
 }
 </script>

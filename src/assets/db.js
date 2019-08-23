773f5c58d2d00cb1e8e85df1021a75a5b3d5547b
Ayrton Fidelis <ayrton.vargas33@gmail.com>
2019-08-23 18:48:19 -0300

    Fix db upgrade

--- src/assets/db.js
+++ src/assets/db.js
@@ -2,13 +2,15 @@ import Dexie from 'dexie'
 
 const db = new Dexie('conselho')
 
-db.version(3).stores({
+db.version(2).stores({
   councils: 'id',
   council_grades: 'id',
   council_topics: 'id',
   council_observation_topics: 'id',
+  evaluations: 'id',
   grades: 'id',
   grade_subjects: 'id',
+  grade_observations: 'id',
   observation_topics: 'id',
   permissions: 'id',
   roles: 'id',
@@ -16,6 +18,33 @@ db.version(3).stores({
   role_type_permissions: 'id',
   schools: 'id',
   students: 'id',
+  student_observations: 'id',
+  student_grades: 'id',
+  subjects: 'id',
+  teachers: 'id',
+  teacher_requests: 'id',
+  topics: 'id',
+  topic_options: 'id',
+  users: 'id'
+})
+
+db.version(3).stores({
+  councils: 'id',
+  council_grades: 'id',
+  council_topics: 'id',
+  council_observation_topics: 'id',
+  evaluations: null,
+  grades: 'id',
+  grade_subjects: 'id',
+  grade_observations: 'id',
+  observation_topics: 'id',
+  permissions: 'id',
+  roles: 'id',
+  role_types: 'id',
+  role_type_permissions: 'id',
+  schools: 'id',
+  students: 'id',
+  student_observations: null,
   student_grades: 'id',
   subjects: 'id',
   teachers: 'id',
